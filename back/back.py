f54b415e624bbeed9339ff7052febfb3e6931b83
yangbin12360 <yb1144149233@163.com>
2023-05-02 22:42:19 +0800

    three.js test版本  back.py 获取原始geoJson数据

--- back/back.py
+++ back/back.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from flask_cors import CORS
+import json
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
 
@@ -8,6 +9,15 @@ CORS(app, supports_credentials=True)
 def helloReact():
     return 'rrr react.'
 
+@app.route('/getJson',methods=["POST"])
+def getJson():
+    mapName = ['boundary','crosswalk','lane','signal','stopline']
+    res = {}
+    for name in mapName:
+        with open('D:/2023ChinaVis/back/data/题目1 高价值场景可视分析/高价值场景可视分析/road10map/'+name+'road10.geojson', 'r', encoding='utf-8') as f:
+            data = json.load(f) 
+        res[name] = data
+    return res 
 
 if __name__ == '__main__':
     # app.debug = True   # 开启调试模式, 代码修改后服务器自动重新载入，无需手动重启
