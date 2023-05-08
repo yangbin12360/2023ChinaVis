4738dd85a002aaa2d627e458949e4dc03026d4b0
LXJ1546 <2991239755@qq.com>
2023-05-08 15:47:00 +0800

    .

--- back/dataProcess.py
+++ back/dataProcess.py
@@ -72,25 +72,25 @@ def dataTypebytime_meas():
         f.write(json.dumps(allData[i]))
 
 
-def sortItem():
-    # 获取文件夹下的所有子文件夹
-    subfolders = [f.path for f in os.scandir(
-        './static/data/DataProcess') if f.is_dir()]
+# def sortItem():
+#     # 获取文件夹下的所有子文件夹
+#     subfolders = [f.path for f in os.scandir(
+#         './static/data/DataProcess') if f.is_dir()]
 
-    # 遍历所有子文件夹，读取子文件夹中的文件
-    for subfolder in subfolders:
-        # 遍历子文件夹中的所有文件
-        for filename in os.listdir(subfolder):
-            filepath = os.path.join(subfolder, filename)
-            # 处理文件
-            with open(filepath, 'r') as f:
-                data = json.load(f)
-                # 处理数据，进行排序
-                data_sorted = sorted(
-                    data, key=operator.itemgetter('time_meas'))
-                # 将修改后的数据写入到原文件
-                with open(filepath, 'w') as f:
-                    json.dump(data_sorted, f)
+#     # 遍历所有子文件夹，读取子文件夹中的文件
+#     for subfolder in subfolders:
+#         # 遍历子文件夹中的所有文件
+#         for filename in os.listdir(subfolder):
+#             filepath = os.path.join(subfolder, filename)
+#             # 处理文件
+#             with open(filepath, 'r') as f:
+#                 data = json.load(f)
+#                 # 处理数据，进行排序
+#                 data_sorted = sorted(
+#                     data, key=operator.itemgetter('time_meas'))
+#                 # 将修改后的数据写入到原文件
+#                 with open(filepath, 'w') as f:
+#                     json.dump(data_sorted, f)
 
 # 提取机动车超速情况
 def Overspeeding():
